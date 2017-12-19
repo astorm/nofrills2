4dca4a22a91d16a4988e8f39e955dd1476a262a5
Alan Storm <astorm@alanstorm.com>
2017-12-18 20:11:57 -0800

    Initial Commit

---
+++ Controller/Chapter1/Index.php
@@ -0,0 +1,20 @@
+<?php
+namespace Pulsestorm\Nofrillslayout\Controller\Chapter1;
+class Index extends \Magento\Framework\App\Action\Action
+{
+
+    protected $resultPageFactory;
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;        
+        parent::__construct($context);
+    }
+    
+    public function execute()
+    {
+        var_dump(__METHOD__);
+        exit;
+    }
+}
