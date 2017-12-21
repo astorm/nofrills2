299c5dc99dea51f0afe16ef6cfd9c634f2961d24
Alan Storm <astorm@alanstorm.com>
2017-12-20 20:40:16 -0800

    Adding

---
+++ Framework/View/Layout.php
@@ -0,0 +1,9 @@
+<?php 
+namespace Pulsestorm\Nofrillslayout\Framework\View;
+class Layout extends \Magento\Framework\View\Layout
+{
+    public function getStructure()
+    {
+        return $this->structure;
+    }
+}
